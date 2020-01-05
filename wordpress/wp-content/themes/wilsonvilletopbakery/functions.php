a625dd6f8daa8def6222cee65b182e7448ab9474
logan <logan.testa@outlook.com>
2020-01-04 17:44:29 -0800

    Set theme to crop medium thumbnails

--- wordpress/wp-content/themes/wilsonvilletopbakery/functions.php
+++ wordpress/wp-content/themes/wilsonvilletopbakery/functions.php
@@ -2,6 +2,14 @@
 
 add_theme_support('post-thumbnails'); //Allow image thumbnails in pages and posts.
 
+//Allow cropping for medium thumbnail images.
+if(false === get_option("medium_crop")) {
+    add_option("medium_crop", "1");
+} else {
+    update_option("medium_crop", "1");
+}
+
+
 add_action('wp_enqueue_scripts', function() {
 
     wp_register_script('toggle-blog-settings-javascript', get_template_directory_uri() . '/assets/javascript/javascript-functions.js');
