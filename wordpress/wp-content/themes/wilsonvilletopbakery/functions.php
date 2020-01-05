841e8a8623b52ac6d7acd8d036e5b64f87b6d8ab
logan <logan.testa@outlook.com>
2020-01-05 12:56:30 -0800

    Adjust theme functions formatting

--- wordpress/wp-content/themes/wilsonvilletopbakery/functions.php
+++ wordpress/wp-content/themes/wilsonvilletopbakery/functions.php
@@ -1,20 +1,19 @@
 <?php
 
-add_theme_support('post-thumbnails'); //Allow image thumbnails in pages and posts.
-
+add_theme_support( "post-thumbnails" ); //Allow image thumbnails in pages and posts.
+//
 //Allow cropping for medium thumbnail images.
-if(false === get_option("medium_crop")) {
-    add_option("medium_crop", "1");
+if(false === get_option( "medium_crop" )) {
+    add_option( "medium_crop", "1" );
 } else {
-    update_option("medium_crop", "1");
+    update_option( "medium_crop", "1" );
 }
-
 
 add_action('wp_enqueue_scripts', function() {
 
-    wp_register_script('toggle-blog-settings-javascript', get_template_directory_uri() . '/assets/javascript/javascript-functions.js');
-    wp_enqueue_script('toggle-blog-settings-javascript', get_template_directory_uri() . '/vue-functions.js');
-    wp_enqueue_style('styles', "" . get_template_directory_uri() . '/assets/css/main-styles.css?mod=12212019');
-    wp_enqueue_style('styles', "" . get_template_directory_uri() . '/assets/css/print-styles.css?mod=12202019');
+    wp_register_script( 'toggle-blog-settings-javascript', get_template_directory_uri() . '/assets/javascript/javascript-functions.js' );
+    wp_enqueue_script( 'toggle-blog-settings-javascript', get_template_directory_uri() . '/vue-functions.js' );
+    wp_enqueue_style( 'styles', "" . get_template_directory_uri() . '/assets/css/main-styles.css?mod=12212019' );
+    wp_enqueue_style( 'styles', "" . get_template_directory_uri() . '/assets/css/print-styles.css?mod=12202019' );
     
 });
