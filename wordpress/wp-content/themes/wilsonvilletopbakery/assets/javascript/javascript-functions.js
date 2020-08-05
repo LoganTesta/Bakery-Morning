0ad8c43bc60a61b237ab39dfc5c6961c87989dc5
logan <logan.testa@outlook.com>
2020-08-05 14:15:22 -0700

    Load JavaScript logic after page load event

--- wordpress/wp-content/themes/wilsonvilletopbakery/assets/javascript/javascript-functions.js
+++ wordpress/wp-content/themes/wilsonvilletopbakery/assets/javascript/javascript-functions.js
@@ -1,17 +1,20 @@
 
-function setCurrentPage(linkNumber) {
-    var navBarDesktop = document.getElementById("desktop-nav");
-    var navBarDesktopItems = navBarDesktop.getElementsByClassName("nav__nav-link");
-    for (var i = 0; i < navBarDesktopItems.length; i++) {
-        navBarDesktopItems[i].className.replace(" current-page", "");
+window.addEventListener("load", function () {
+    function setCurrentPage(linkNumber) {
+        var navBarDesktop = document.getElementById("desktop-nav");
+        var navBarDesktopItems = navBarDesktop.getElementsByClassName("nav__nav-link");
+        for (var i = 0; i < navBarDesktopItems.length; i++) {
+            navBarDesktopItems[i].className.replace(" current-page", "");
+        }
+        navBarDesktopItems[linkNumber].className += " current-page";
     }
-    navBarDesktopItems[linkNumber].className += " current-page";
-}
 
 
-var dropdownButton=document.getElementById("dropdownButton");
-dropdownButton.addEventListener("click", toggleHamburgerMenu, "false");
+    var dropdownButton = document.getElementById("dropdownButton");
+    dropdownButton.addEventListener("click", toggleHamburgerMenu, "false");
 
-function toggleHamburgerMenu (){
-    document.getElementById("dropdownContent").classList.toggle("show");
-}
+    function toggleHamburgerMenu() {
+        document.getElementById("dropdownContent").classList.toggle("show");
+    }
+
+}, "false");
