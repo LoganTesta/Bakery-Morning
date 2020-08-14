921846fb492757a9c2acb343e1e31c6a59bb3c3a
logan <logan.testa@outlook.com>
2020-08-13 18:03:53 -0700

    Style blog page posts

--- index.php
+++ index.php
@@ -77,7 +77,6 @@
                                         ?>
                                         <div class="blog">
                                             <div class="blog__title"><?php the_title(); ?></div>
-                                            <div class="blog__date"><?php the_date(); ?></div>
                                             <div class="blog__categories">
                                                 <?php
                                                 $categories = get_the_category();
@@ -99,6 +98,7 @@
                                                 }
                                             ?>
                                             </div>
+                                            <div class="blog__date"><?php the_date(); ?></div>
                                             <div class="blog__content"><?php the_content(); ?></div>
                                             <?php if( has_post_thumbnail() ) { echo "<div class='blog__image' style=\"background-image: url('" . get_the_post_thumbnail_url() . "')\"></div>"; } ?>
                                        </div>
