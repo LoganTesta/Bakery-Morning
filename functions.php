621b3476f4e83cea47a8cf9e89e9fd56306a593f
logan <logan.testa@outlook.com>
2020-08-24 13:56:04 -0700

    Add theme support for different post formats

--- functions.php
+++ functions.php
@@ -26,6 +26,7 @@ add_action( 'wp_enqueue_scripts', function() {
 
 //Theme support.
 add_theme_support( "post-thumbnails" ); //Allow image thumbnails in pages and posts.
+add_theme_support( "post-formats", array( 'aside', 'gallery', 'quote', 'image', 'video' ) ); //Let user customize the post format.  
 add_theme_support( 'custom-logo' );   //Let user upload the logo.
 
 
