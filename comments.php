50b4e1ce870944d6c0b246c4c55522d863468902
logan <logan.testa@outlook.com>
2020-09-02 18:20:00 -0700

    Switch comments to use wp_list_comments();

--- comments.php
+++ comments.php
@@ -11,14 +11,8 @@ $comments = $comments_query->query( $args );
 
 if ( $comments && have_comments() ) {
     echo "<div class='comments'>";
-    echo "<h3 class='comments__header'>Comments</h3>";
-    foreach ( $comments as $comment ) {
-        echo "<div class='comments__comment'>";
-        echo "<div class='comments__comment__body'>" . $comment->comment_content . '</div>';
-        echo "<div class='comments__comment__author'>-" . $comment->comment_author . ':</div>';
-        echo "<div class='comments__comment__date'>" . get_comment_date( 'M d, Y' ) . ' at ' . get_comment_date( 'g:ha' ) . '</div>';
-        echo "</div>";
-    }  
+        echo "<h3 class='comments__header'>Comments</h3>";
+        wp_list_comments();
     echo "</div>";
 } else {
     echo "<div class='comments'>No comments yet.</div>"; 
