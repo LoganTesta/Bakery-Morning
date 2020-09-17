eb524ce18211cc4184c6c21733c3112b2b2f10f9
logan <logan.testa@outlook.com>
2020-09-17 16:26:14 -0700

    Improve web and print styling for blog page

--- index.php
+++ index.php
@@ -55,8 +55,8 @@ $withcomments = "1";
                                             <div class="blog__tags"><?php the_tags(); ?></div>
                                             <div class="blog__author">By: <?php the_author(); ?></div>
                                             <div class="blog__date"><?php echo get_the_date(); ?></div>
-                                            <div class="blog__content"><?php the_content(); ?></div>
                                             <?php if( has_post_thumbnail() ) { echo "<div class='blog__image' style=\"background-image: url('" . get_the_post_thumbnail_url() . "')\"></div>"; } ?>
+                                            <div class="blog__content"><?php the_content(); ?></div>                                          
                                             <?php 
                                                 if( comments_open() ) {
                                                     comments_template();
