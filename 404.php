7604cdefe23e5f943ca21d284721437cd1cdbc50
logan <logan.testa@outlook.com>
2020-09-18 13:06:59 -0700

    Refactor 404 page

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@
                             <div class="content__body">
                                 <p>It looks like that page or link is not found.  Please select one of the links in the navbar to get to
                                     another page in our site.</p>
-                                <div class="content__featured-image <?php if( trim( the_post_thumbnail_url() ) === "" ) { echo "hide"; } ?>" style="background-image: url('<?php echo the_post_thumbnail_url(); ?>')"></div>
+                                <div class="content__featured-image <?php if( esc_attr( trim( the_post_thumbnail_url() ) ) === "" ) { echo "hide"; } ?>" style="background-image: url('<?php echo esc_attr( the_post_thumbnail_url() ); ?>')"></div>
                             </div>  
                         </div>
                     </div>  
