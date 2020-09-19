28ded9a71394968499bd1e51cc028c3f01ec6962
logan <logan.testa@outlook.com>
2020-09-19 12:21:45 -0700

    Refactor index page to use WP_Query

--- index.php
+++ index.php
@@ -80,45 +80,50 @@ $withcomments = "1";
                         <div class="content-row">
                             <h3 class="index-content__subheading"><?php echo esc_html( get_theme_mod( 'index_blog_heading_code' ) ); ?></h3>
                             <?php global $post;
+                            $index_page_query;
                             if( get_theme_mod( 'index_blog_order_code' ) === 'desc' ) {
-                                $args = array( 'posts_per_page' => 4, 'offset' => 0, 'orderBy' => 'date', 'order' => get_theme_mod( 'index_blog_order_code' ) ); 
+                                $index_page_query = new WP_Query( 
+                                    array( 'posts_per_page' => 4, 'offset' => 0, 'orderBy' => 'date', 'order' => get_theme_mod( 'index_blog_order_code' ) 
+                                ) ); 
                             } else {
-                                $args = array( 'posts_per_page' => 4, 'offset' => wp_count_posts( 'post' )->publish - 4, 'orderBy' => 'date', 'order' => get_theme_mod( 'index_blog_order_code' ) );     
+                                $index_page_query = new WP_Query( 
+                                    array( 'posts_per_page' => 4, 'offset' => wp_count_posts( 'post' )->publish - 4, 'orderBy' => 'date', 'order' => get_theme_mod( 'index_blog_order_code' ) 
+                                ) );     
                             }
-                            $postsToDisplay = get_posts( $args );
-                            foreach ( $postsToDisplay as $post ) : setup_postdata( $post );
-                                ?>      
-                                <div class="col-sma-3">
-                                    <div class="index__blog">
-                                        <h4 class="index__blog__title"><a href="blog#<?php the_title(); ?>" class="index__blog__title__link"><?php the_title(); ?></a></h4>
-                                        <div class="index__blog__categories"><?php
-                                            $categories = get_the_category();
-                                            $h = 0;
-                                            foreach ( $categories as $category ) {
-                                                $h++;
-                                            }
-                                            $h = $h - 1;
-                                            $i = 0;
-                                            foreach ( $categories as $category ) {
-                                                $result = "";
-                                                if ( $i < $h ) {
-                                                    $result .= $category->name . ", ";
-                                                } else {
-                                                    $result .= $category->name;
+                            if( $index_page_query->have_posts() ) {
+                                while( $index_page_query->have_posts() ) : $index_page_query->the_post(); ?>   
+                                    <div class="col-sma-3">
+                                        <div class="index__blog">
+                                            <h4 class="index__blog__title"><a href="blog#<?php the_title(); ?>" class="index__blog__title__link"><?php the_title(); ?></a></h4>
+                                            <div class="index__blog__categories"><?php
+                                                $categories = get_the_category();
+                                                $h = 0;
+                                                foreach ( $categories as $category ) {
+                                                    $h++;
                                                 }
-                                                echo esc_html( $result );
-                                                $i++;
-                                            }
-                                            ?>
+                                                $h = $h - 1;
+                                                $i = 0;
+                                                foreach ( $categories as $category ) {
+                                                    $result = "";
+                                                    if ( $i < $h ) {
+                                                        $result .= $category->name . ", ";
+                                                    } else {
+                                                        $result .= $category->name;
+                                                    }
+                                                    echo esc_html( $result );
+                                                    $i++;
+                                                }
+                                                ?>
+                                            </div>
+                                            <div class="index__blog__author">By <?php the_author(); ?></div>
+                                            <div class="index__blog__date"><?php echo get_the_date(); ?></div>
+                                            <?php if( has_post_thumbnail() ) { echo "<div class='index__blog__image' style=\"background-image: url('" . esc_url( get_the_post_thumbnail_url() ) . "')\"><a class='index__blog__link' href='blog#" . esc_url( get_the_title() ) . "'></a></div>"; } ?>
+                                            <div class="index__blog__content"><?php the_excerpt(); ?></div>
+                                            <div class="clear-both"></div>
                                         </div>
-                                        <div class="index__blog__author">By <?php the_author(); ?></div>
-                                        <div class="index__blog__date"><?php echo get_the_date(); ?></div>
-                                        <?php if( has_post_thumbnail() ) { echo "<div class='index__blog__image' style=\"background-image: url('" . esc_url( get_the_post_thumbnail_url() ) . "')\"><a class='index__blog__link' href='blog#" . esc_url( get_the_title() ) . "'></a></div>"; } ?>
-                                        <div class="index__blog__content"><?php the_excerpt(); ?></div>
-                                        <div class="clear-both"></div>
                                     </div>
-                                </div>
-                            <?php endforeach; ?>  
+                                <?php endwhile;
+                            } ?>
                         </div>
                     <?php } ?>
                 </div>
