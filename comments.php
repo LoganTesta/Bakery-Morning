879379ea2da9baf84425c06e3b23b36feaf1afa1
logan <logan.testa@outlook.com>
2020-09-25 16:19:24 -0700

    Refactor styles, increase comment avatar size

--- comments.php
+++ comments.php
@@ -12,7 +12,7 @@ $selected_comments = $comments_query->query( $args );
 if ( $selected_comments && have_comments() ) {
     echo "<div id='comments' class='comments'>";
         echo "<h3 class='comments__header'>Comments</h3>";
-        wp_list_comments( array( 'per_page' => '6' ) );
+        wp_list_comments( array( 'avatar_size' => '60' ) );
         the_comments_pagination( array( 'prev_text' => __( '<< Prev', 'bakery-morning' ), 'next_text' => __( 'Next >>', 'bakery-morning' ) ) );
     echo "</div>";
 } else {
