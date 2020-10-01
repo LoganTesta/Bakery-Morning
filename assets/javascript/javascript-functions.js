ac74760c8f6096e7069475602cb77331cb741bb9
logan <logan.testa@outlook.com>
2020-09-30 20:29:49 -0700

    Add mobile dropdown for submenus

--- assets/javascript/javascript-functions.js
+++ assets/javascript/javascript-functions.js
@@ -7,5 +7,27 @@ window.addEventListener("load", function () {
     function toggleHamburgerMenu() {
         document.getElementById("dropdownContent").classList.toggle("show");
     }
+    
+    
+    
+    var theArrows = document.querySelectorAll("li.page_item_has_children, li.menu-item-has-children"); 
+    for(let i = 0; i < theArrows.length; i++) {
+        theArrows[i].addEventListener("click", function(){
+            showMenu(i);
+        }, "false");
+    }
+
+    function showMenu(itemNumber) {
+        var theSubMenus = theArrows[itemNumber].querySelectorAll(".sub-menu")
+        theSubMenus[0].classList.toggle("show");
+        
+        for(let i = 0; i < theSubMenus.length; i++){
+            if(theSubMenus[0].classList.contains("show")){
+                theSubMenus[i].classList.add("show");
+            } else {
+                theSubMenus[i].classList.remove("show");
+            }
+        }   
+    }
 
 }, "false");
