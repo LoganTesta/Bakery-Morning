f3d112d2a376ef6d5c9b28353c1be8b15528b862
logan <logan.testa@outlook.com>
2022-04-22 22:21:06 -0700

    Limit font-size auto resizing

--- functions.php
+++ functions.php
@@ -86,7 +86,7 @@ add_action( 'wp_enqueue_scripts', function() {
  
     wp_register_script( 'javascript-functions', get_template_directory_uri() . '/assets/javascript/javascript-functions.js' );
     wp_enqueue_script( 'javascript-functions', get_template_directory_uri() . '/assets/javascript/javascript-functions.js' );  
-    wp_enqueue_style( 'styles', "" . get_template_directory_uri() . '/assets/css/main-styles.css?mod=09172020' );
+    wp_enqueue_style( 'styles', "" . get_template_directory_uri() . '/assets/css/main-styles.css?mod=04222022' );
     wp_enqueue_style( 'print-styles', "" . get_template_directory_uri() . '/assets/css/print-styles.css?mod=09172020' );   
 });
 
